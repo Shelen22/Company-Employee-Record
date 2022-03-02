67126386d7b65b17c2b105dd40d5e90af5f9c9e9
Shailendra Singh <shailendrauday5@gmail.com>
2022-03-02 20:29:01 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,30 +1,29 @@
 import React, { useEffect, useState } from "react";
 // import Pagination from '@mui/material/Pagination';
- import("./Home.css")
+import("./Home.css");
 
 const Home = () => {
   const [employees, setEmployees] = useState([]);
   const [pages, setPages] = useState(0);
   const [num, setNum] = useState(1);
-  const [details , setDetails] = useState(false);
-  const [inputchar , setInputChar] = useState("");
+  const [details, setDetails] = useState(false);
+  const [inputchar, setInputChar] = useState("");
   // const [gender , setGender] = useState("all");
 
   useEffect(() => {
     getEmployee();
   }, [num]);
-        
+
   // useEffect(() => {
   //   getEmployee2();
   // }, [gender]);
-  
-   
+
   const SearchUser = () => {
     fetch(`http://localhost:2244/employees/${inputchar}`)
       .then((res) => res.json())
-      .then((res) => {  
+      .then((res) => {
         // console.log('res.Employees:', res[0])
-        setEmployees(res);   
+        setEmployees(res);
         setPages(res.totalPages);
       })
       .catch((err) => {
@@ -38,7 +37,7 @@ const Home = () => {
       .then((res) => {
         //  console.log('res:', res.Employees[0)
 
-        // console.log("res.totalpages:", res.totalpages);  
+        // console.log("res.totalpages:", res.totalpages);
         setEmployees(res.Employees);
         setPages(res.totalPages);
       })
@@ -53,7 +52,7 @@ const Home = () => {
   //     .then((res) => {
   //       //  console.log('res:', res.Employees[0)
 
-  //       // console.log("res.totalpages:", res.totalpages);  
+  //       // console.log("res.totalpages:", res.totalpages);
   //       setEmployees(res.Employees);
   //       setPages(res.totalPages);
   //     })
@@ -62,7 +61,6 @@ const Home = () => {
   //     });
   // };
 
-  
   var pg = [];
   function pag() {
     for (let i = 1; i <= pages; i++) {
@@ -72,20 +70,32 @@ const Home = () => {
   }
   pag();
   return (
-    <> 
-    <div>
-     <div style = {{display: 'flex' , alignItems: 'center', justifyContent: 'center'}}>
-        <input type="text" onChange={(e) => setInputChar(e.target.value)} />
-        
-         <button onClick={() => {SearchUser()}}>Submit</button>
-    </div>
-          {/* <select onChange={(e) => setGender(e.target.value)} >
+    <>
+      <div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <input type="text" onChange={(e) => setInputChar(e.target.value)} />
+
+          <button
+            onClick={() => {
+              SearchUser();
+            }}
+          >
+            Submit
+          </button>
+        </div>
+        {/* <select onChange={(e) => setGender(e.target.value)} >
               <option value={'all'}> select </option>
               <option value={'Male'}> Male</option>
               <option value={'Female'}> Female </option>
           </select> */}
-    </div>
-      
+      </div>
+
       {employees.map((e, i) => (
         <div
           key={i}
@@ -94,42 +104,50 @@ const Home = () => {
             width: "60%",
             margin: " 10px auto",
             borderRadius: "10px",
-            padding : "8px",
-            display : " flex",
-            alignItems: "center",
-            cursor: "pointer",
+            padding: "8px",
+            display: " flex",
+              alignItems: "center"
           }}
-          onClick = {() => setDetails(!details) }
         >
-          <div>
-            <img
-              src={e.profile}
-              style={{ width: "100px", borderRadius: "50%" }}
-              alt={e.name}
-            />
+          <div
+            style={{
+              display: " flex",
+              alignItems: "center",
+              cursor: "pointer"
+            }}
+            onClick={() => setDetails(!details)}
+          >
+            <div>
+              <img
+                src={e.profile}
+                style={{ width: "100px", borderRadius: "50%" }}
+                alt={e.name}
+              />
+            </div>
+            <div style={{ marginLeft: "25px" }}>
+              <h3 style={{ marginBottom: "0px" }}>{e.name}</h3>
+              <p style={{ margin: "5px 0px" }}>
+                Joining Date : {e.joiningDate}
+              </p>
+              <p style={{ margin: "5px 0px" }}>Department : {e.department}</p>
+            </div>
           </div>
-          <div style={{marginLeft : "25px"}}>
-            <h3 style={{marginBottom : "0px"}}>{e.name}</h3>
-            <p style={{margin : "5px 0px"}}>Joining Date : {e.joiningDate}</p>
-            <p style={{margin : "5px 0px"}}>Department : {e.department}</p>
-          </div>
-          {details ?
-           <table style={{margin : "auto"}} >
-             <tbody >
-
-             <tr>
-               <th>Month</th>
-               <th>Year</th>
-               <th>Salary</th>
-             </tr>
-             <tr>
-               <td>{e.payment[0].month} Month</td>
-               <td>{e.payment[0].year}</td>
-               <td>{e.payment[0].salary}-/Rs</td>
-             </tr>
-             </tbody>
-           </table> : null
-          }
+          {details ? (
+            <table style={{ margin: "auto" }}>
+              <tbody>
+                <tr>
+                  <th>Month</th>
+                  <th>Year</th>
+                  <th>Salary</th>
+                </tr>
+                <tr>
+                  <td>{e.payment[0].month} Month</td>
+                  <td>{e.payment[0].year}</td>
+                  <td>{e.payment[0].salary}-/Rs</td>
+                </tr>
+              </tbody>
+            </table>
+          ) : null}
         </div>
       ))}
       <div style={{ margin: "auto", width: "10%" }}>
@@ -147,10 +165,8 @@ const Home = () => {
             })
           : null}
       </div>
-      
     </>
   );
 };
 
 export default Home;
-
